404ecbb37e63d5561226ebe51c07c7fa3db491e2
Joshua Blount <hi@joshuablount.com>
2024-06-03 12:46:46 -0700

    building rowing / music likes script out, needs to get moved around / rethought, but functional now

--- lobbler/listens/management/commands/getfaves.py
+++ lobbler/listens/management/commands/getfaves.py
@@ -1,5 +1,12 @@
 import json
 import time
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv("../../../../.env")  
+ROWING_KEY = str(os.getenv('ROWING_KEY'))
+
 from ytmusicapi import YTMusic
 
 # This is currently just a standalone pythong script, but it uses all the stuff
@@ -8,6 +15,13 @@ from ytmusicapi import YTMusic
 yt = YTMusic('oauth.json')
 likes = yt.get_liked_songs()
 likes['tracks'] = likes['tracks'][:10]
-timestr = time.strftime("%Y%m%d-%H%M%S") + ".json"
+timestr = time.strftime("musiclikes-%Y%m%d-%H%M%S") + ".json"
 with open(timestr, 'w', encoding='utf-8') as f:
     json.dump(likes, f, ensure_ascii=False, indent=4)
+
+
+response = requests.get('https://log.concept2.com/api/users/me/results', headers={'Authorization': 'Bearer ' + ROWING_KEY})
+
+timestr = time.strftime("rowing-%Y%m%d-%H%M%S") + ".json"
+with open(timestr, 'w', encoding='utf-8') as f:
+    json.dump(response.json()['data'][:10], f, ensure_ascii=False, indent=4)
