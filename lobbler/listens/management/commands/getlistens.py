2066bb18657ec0320b4e190a773f44ea91c23e0f
Joshua Blount <hi@joshuablount.com>
2024-06-05 08:48:06 -0700

    quick refactor to a slightly less onerous if statement

--- lobbler/listens/management/commands/getlistens.py
+++ lobbler/listens/management/commands/getlistens.py
@@ -1,7 +1,5 @@
 from datetime import datetime
-import json
 from ytmusicapi import YTMusic
-import time
 import pytz
 
 from django.core.management.base import BaseCommand #, CommandError
@@ -14,36 +12,27 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         tz_local = pytz.timezone('America/Los_Angeles')
-        count = 0
-        # for comparison to see what we have
         yt = YTMusic('oauth.json')
         listens = yt.get_history()
         listens.reverse()
 
         for listen in listens:
-            if (listen['played'] == 'Yesterday'):
-                print(listen['title'])
 
             artist, artist_created = Artist.objects.get_or_create(name=listen['artists'][0]['name'])
 
             if listen['album']:
                 release, album_created = Release.objects.get_or_create(artist=artist, name=listen['album']['name'])
-                song, song_created = Song.objects.get_or_create(
-                    name=listen['title'],
-                    artist=artist,
-                    length=listen['duration_seconds'],
-                    videoId=listen['videoId'],
-                    release=release
-                )
             else:
-                song, song_created = Song.objects.get_or_create(
-                    name=listen['title'],
-                    videoId=listen['videoId'],
-                    length=listen['duration_seconds'],
-                    artist=artist
-                )
+                release = False
+            song, song_created = Song.objects.get_or_create(
+                name=listen['title'],
+                artist=artist,
+                length=listen['duration_seconds'],
+                videoId=listen['videoId'],
+                release=release
+            )
 
+            this_listen = Listen(song=song, played=listen['played'], when=datetime.now(tz=tz_local), json_from_source=str(listen))
+            this_listen.save()
 
-            if listen['played'] == 'Yesterday':
-                this_listen = Listen(song=song, played=listen['played'], when=datetime.now(tz=tz_local), json_from_source=str(listen))
-                this_listen.save()
+            print("Now we have %i" % Listen.objects.all().count())
